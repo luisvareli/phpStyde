9176c869366d401c36943d5902879ca1abf1c810
Luis Vargas <lewiis29@gmail.com>
2020-10-22 12:33:18 -0600

    Instalación y uso de componentes de terceros con Composer (Carbon)

---
+++ composer2/src/User.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Styde;
+
+use Exception;
+
+use Carbon\Carbon;
+
+class User extends Model
+{
+    public function getAgeAttribute()
+    {
+        $date = Carbon::createFromFormat('d/m/Y', $this->birthDate);
+
+        return $date->age;
+    }
+}
+
+
